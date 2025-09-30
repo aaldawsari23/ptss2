c1a117a145a2cdaf3b558ac32599938f6e1ac95e
aaldawsari23 <pt.aldawsari@gmail.com>
2025-09-30 05:49:40 +0300

    Delete hhc-app directory

--- hhc-app/app/dashboard/visits/page.tsx
+++
@@ -1,8 +0,0 @@
-// app/dashboard/visits/page.tsx
-// Placeholder page for the Visits feature. This page will display a list of
-// scheduled visits and allow staff to manage them. It will be implemented
-// in a future phase.
-
-export default function VisitsPage() {
-  return <div>Visits management coming soon.</div>;
-}
